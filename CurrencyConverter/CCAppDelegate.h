f139424709e7c913777c85e9cf2828f855b2e073
Tyler Hedrick <tylerhedrick@me.com>
2013-03-21 22:59:32 -0400

    Initial Commit

---
+++ CurrencyConverter/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Tyler Hedrick on 3/21/13.
+//  Copyright (c) 2013 hedrick.tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CurrencyConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CurrencyConverter
+//
+//  Created by Tyler Hedrick on 3/21/13.
+//  Copyright (c) 2013 hedrick.tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CCAppDelegate class]));
+    }
+}
